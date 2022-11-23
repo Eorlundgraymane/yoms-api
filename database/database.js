d18fda5cda0dd93485cee446e7362c13738b6e05
Code Rider <rkmenon235@gmail.com>
2022-11-24 01:55:40 +0530

    DB Change

--- database/database.js
+++ database/database.js
@@ -1,19 +1,19 @@
 const { Sequelize } = require("sequelize");
 
-const database = new Sequelize("yoms", "root", "password", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// const database = new Sequelize("yoms", "root", "password", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
-// const database = new Sequelize(
-//   "rtuah3x8s0df08v4",
-//   "m1vbx24mz5q3h2in",
-//   "ahrh2yxo8arv441u",
-//   {
-//     dialect: "mysql",
-//     host: "qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//     port: 3306,
-//   }
-// );
+const database = new Sequelize(
+  "zvr5vsjzyrwv48x7",
+  "tkbh4nhxzlqlzsgm",
+  "tpun81wjmbwb3agt",
+  {
+    dialect: "mysql",
+    host: "	lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+  }
+);
 
 module.exports = database;
