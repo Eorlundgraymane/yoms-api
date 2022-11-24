6aeda774975cfd6ed465bde902519f9dcbf42281
Code Rider <rkmenon235@gmail.com>
2022-11-24 11:56:07 +0530

    DB Cred Fix

--- database/database.js
+++ database/database.js
@@ -6,9 +6,9 @@ const { Sequelize } = require("sequelize");
 // });
 
 const database = new Sequelize(
+  "tpun81wjmbwb3agt",
   "zvr5vsjzyrwv48x7",
   "tkbh4nhxzlqlzsgm",
-  "tpun81wjmbwb3agt",
   {
     dialect: "mysql",
     host: "lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
