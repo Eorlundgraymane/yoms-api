f861bd61ddb6b7b0a19219347694cd3130bad328
Eorlund Graymane <rkmenon235@gmail.com>
2023-08-13 03:58:03 +0530

    DB Update

--- database/database.js
+++ database/database.js
@@ -1,16 +1,30 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("yoms-api", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-});
-sequelize
-  .authenticate()
-  .then((result) => {
+const dbConnectionString = process.env.JAWSDB_URL;
+if (dbConnectionString != null) {
+  const herokuSequelize = new Sequelize(dbConnectionString)
+  herokuSequelize.authenticate().then((result) => {
     console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
+  }).catch((error) => {
     console.error("Unable to connect to the database: ", error);
   });
+}
+else {
+  const sequelize = new Sequelize("yoms-api", "root", "password", {
+    host: "localhost",
+    dialect: "mysql",
+  });
+  sequelize
+    .authenticate()
+    .then((result) => {
+      console.log("Connection has been established successfully.");
+    })
+    .catch((error) => {
+      console.error("Unable to connect to the database: ", error);
+    });
 
-module.exports = sequelize;
+  module.exports = sequelize;
+}
+
+
+
